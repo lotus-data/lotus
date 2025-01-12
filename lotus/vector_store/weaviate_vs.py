7b5dfd375ffa19a9e70fc252180755d5b3e7d28c
Amogh Tantradi <amogh_t2003@berkeley.edu>
2025-01-12 14:09:02 -0800

    refactored

--- lotus/vector_store/weaviate_vs.py
+++ lotus/vector_store/weaviate_vs.py
@@ -1,13 +1,13 @@
-from typing import Optional, Union
-
 from lotus.vector_store.vs import VS
-
-try:
-    import weaviate
-except ImportError:
-    raise ImportError("Please install the weaviate client")
 
 
 class WeaviateVS(VS):
-    def __init__(self, weaviate_collection_name:str, weaviate_client: Union[weaviate.WeaviateClient, weaviate.Client], weaviate_collection_text_key: Optional[str] = "content"):
+    def __init__(self):
+        try:
+            import weaviate
+        except ImportError:
+            weaviate = None 
+            
+        if weaviate is None:
+            raise ImportError("Please install the weaviate client")
         pass
