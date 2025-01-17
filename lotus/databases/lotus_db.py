cc4ddcfb352cb4143474cfac42ef82d77e21aa6f
StanChan03 <StanleyChan529@gmail.com>
2025-01-16 16:44:12 -0800

    improve logging

--- lotus/databases/lotus_db.py
+++ lotus/databases/lotus_db.py
@@ -2,6 +2,8 @@ from typing import Optional
 
 import pandas as pd
 
+import lotus
+
 
 class LotusDB:
     def __init__(self, connector):
@@ -21,13 +23,21 @@ class LotusDB:
             pd.DataFrame: The result of the query
 
         """
-        if db_type == "sql":
-            return pd.read_sql(query, self.connector.sql_engine)
-        elif db_type == "nosql":
-            if not collection_name or not db_name:
-                raise ValueError("Collection name and database is required for NoSQL database")
-            collection = self.connector.nosql_client[db_name][collection_name]
-            results = collection.find(query)
-            return pd.DataFrame(list(results))
-        else:
-            raise ValueError("Invalid database type")
+        try:
+            if db_type == "sql":
+                if not isinstance(query, str):
+                    raise ValueError("Query must be a string")
+                lotus.logger.debug("Executing SQL Query")
+                return pd.read_sql(query, self.connector.sql_engine)
+            elif db_type == "nosql":
+                if not collection_name or not db_name:
+                    raise ValueError("Collection name and database is required for NoSQL database")
+                collection = self.connector.nosql_client[db_name][collection_name]
+                results = collection.find(query)
+                return pd.DataFrame(list(results))
+            else:
+                raise ValueError("Invalid database type")
+
+        except Exception as e:
+            lotus.logger.error(f"Error executing query: {e}")
+            raise
