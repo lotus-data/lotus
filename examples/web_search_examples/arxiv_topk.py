e53cd65ea66e213b6272e94b816415a8beef8621
liana313 <lianapat@stanford.edu>
2025-02-10 09:02:49 -0800

    minor changes to examples

--- examples/web_search_examples/arxiv_topk.py
+++ examples/web_search_examples/arxiv_topk.py
@@ -7,6 +7,7 @@ lm = LM(model="gpt-4o-mini")
 lotus.settings.configure(lm=lm)
 
 df = web_search(WebSearchCorpus.ARXIV, "deep learning", 5)[["title", "abstract"]]
-print(f"Results from Arxiv\n{df}")
-most_interesting_articles = df.sem_topk("Which {abstract} is most interesting?", K=1)
-print(f"Most interesting articles\n{most_interesting_articles}")
+print(f"Results from Arxiv\n{df}\n\n")
+
+most_interesting_articles = df.sem_topk("Which {abstract} is most exciting?", K=1)
+print(f"Most interesting article: \n{most_interesting_articles.iloc[0]}")
--- examples/web_search_examples/google_topk.py
+++ examples/web_search_examples/google_topk.py
@@ -6,7 +6,7 @@ lm = LM(model="gpt-4o-mini")
 
 lotus.settings.configure(lm=lm)
 
-df = web_search(WebSearchCorpus.GOOGLE, "deep learning", 5)[["title", "snippet"]]
+df = web_search(WebSearchCorpus.GOOGLE, "deep learning research", 5)[["title", "snippet"]]
 print(f"Results from Google\n{df}")
-most_interesting_articles = df.sem_topk("Which {snippet} is most interesting?", K=1)
+most_interesting_articles = df.sem_topk("Which {snippet} is the most exciting?", K=1)
 print(f"Most interesting articles\n{most_interesting_articles}")
